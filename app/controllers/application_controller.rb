74ad4a5f2326beb7e4509e19c99382c1072e74b1
Vaibhao Falode <vaibhaofalode@gmail.com>
2019-05-10 13:31:05 +0530

    Update:
    - Installed the haml gem in the project.
    - Added bootstrap and devise in the project.
    - Updated the UI for the sign and sign up pages.
    - Created new home page controller with its view files.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+    protected
+    def after_sign_in_path_for(resource)
+      home_index_path
+    end
+  
+    # def after_sign_out_path_for(resource)
+    #   # return the path based on resource
+    # end  
 end
